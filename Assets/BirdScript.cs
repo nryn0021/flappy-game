737943d487dfc2dfcb6790e8340e6aae1c7fdd99
nryn0021 <80402194+nryn0021@users.noreply.github.com>
2023-07-30 14:09:44 +0530

    Project FIles
    1st try

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>(); //it is used to connect to LogicScript code.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive) // || Input.GetKeyDown(KeyCode.UpArrow)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+        /*else if (Input.GetKeyDown(KeyCode.DownArrow) == true)
+        {
+            myRigidbody.velocity = Vector2.down * 10;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+        {
+            myRigidbody.velocity = Vector2.right * 10;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+        {
+            myRigidbody.velocity = Vector2.left * 10;
+        }*/
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
